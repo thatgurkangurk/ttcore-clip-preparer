67970b9634fbbfe1404421866c523e8a27949275
Gurkan <hello@gurkz.me>
2026-02-26 18:31:17 +0100

    create out dir in main

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,20 @@ use clap::Parser;
 use crate::cli::Cli;
 use crate::config::Config;
 
+async fn ensure_out_dir_exists(config: &Config) -> Result<(), Box<dyn std::error::Error>> {
+    let path = &config.fs.out_dir;
+
+    tokio::fs::create_dir_all(&path).await?;
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::try_parse().expect("expected cli to parse");
     let config = Config::load().expect("expected config to load");
+
+    ensure_out_dir_exists(&config).await?;
 
     match cli.command {
         Some(cli::Commands::Download { video_id }) => {
@@ -18,7 +28,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
         Some(cli::Commands::Clean) => {
             let path = config.fs.out_dir;
-            let mut entries = tokio::fs::read_dir(path).await?;
+
+            let mut entries = tokio::fs::read_dir(&path).await?;
 
             while let Some(entry) = entries.next_entry().await? {
                 let path = entry.path();
