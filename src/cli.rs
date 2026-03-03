fefdab1a7c224219e8ec3e891a3d6e15730848c3
Gurkan <hello@gurkz.me>
2026-03-03 13:24:22 +0100

    simplify main.rs more (move command execution into cli.rs)

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,16 @@
 use std::path::PathBuf;
 
+use anyhow::{Context, Result};
 use clap::{Parser, Subcommand};
+use reqwest::Client;
+
+use crate::{
+    burner,
+    config::Config,
+    download,
+    fs::{clean_burned_dirs, clean_output_dir, ensure_out_dir_exists},
+    update,
+};
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
@@ -28,4 +38,57 @@ pub enum Commands {
     Clean,
     CleanBurned,
     Update,
+}
+
+impl Commands {
+    pub async fn execute(self, config_path: Option<PathBuf>) -> Result<()> {
+        match self {
+            Commands::Update => {
+                update::update()?;
+                return Ok(());
+            }
+
+            command => {
+                let config =
+                    Config::load(config_path.as_deref()).context("failed to load configuration")?;
+
+                ensure_out_dir_exists(&config)
+                    .await
+                    .context("failed to ensure output directory exists")?;
+
+                match command {
+                    Commands::Download { video_id } => {
+                        let client = Client::new();
+
+                        download::download_selected_files(video_id, &config, &client)
+                            .await
+                            .context("download command failed")?;
+                    }
+
+                    Commands::BurnCredits { video_id, crf } => {
+                        let path = config.fs.out_dir.join(video_id.to_string());
+
+                        burner::burn_multiline_text_batch(path, config.fs.font_file, crf)
+                            .context("failed to burn credits text")?;
+                    }
+
+                    Commands::Clean => {
+                        clean_output_dir(&config)
+                            .await
+                            .context("failed to clean output directory")?;
+                    }
+
+                    Commands::CleanBurned => {
+                        clean_burned_dirs(&config.fs.out_dir)
+                            .await
+                            .context("failed to clean burned directories")?;
+                    }
+
+                    Commands::Update => unreachable!(),
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,52 +8,16 @@ mod update;
 
 use clap::Parser;
 
-use crate::config::Config;
-use crate::{cli::Cli, fs::clean_output_dir};
-use anyhow::{Context, Result};
+use anyhow::Result;
 
-use crate::fs::{clean_burned_dirs, ensure_out_dir_exists};
+use crate::cli::Cli;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let cli = Cli::parse();
 
-    if matches!(cli.command, Some(cli::Commands::Update)) {
-        crate::update::update()?;
-        return Ok(());
-    }
-
-    let config = Config::load(cli.config.as_deref()).context("failed to load configuration")?;
-    let reqwest_client = reqwest::Client::new();
-
-    ensure_out_dir_exists(&config)
-        .await
-        .context("failed to ensure that the output directory exists")?;
-
-    match cli.command {
-        Some(cli::Commands::Download { video_id }) => {
-            download::download_selected_files(video_id, &config, &reqwest_client)
-                .await
-                .context("download command failed")?;
-        }
-        Some(cli::Commands::BurnCredits { video_id, crf }) => {
-            let path = config.fs.out_dir.join(video_id.to_string());
-            crate::burner::burn_multiline_text_batch(path, config.fs.font_file, crf)
-                .context("failed to burn credits text")?;
-        }
-        Some(cli::Commands::Clean) => clean_output_dir(&config)
-            .await
-            .context("failed to clean the output directory")?,
-        Some(cli::Commands::CleanBurned) => {
-            let path = config.fs.out_dir;
-
-            clean_burned_dirs(&path)
-                .await
-                .context("Failed to clean burned directories")?;
-        }
-        None => {}
-        // already handled above
-        Some(cli::Commands::Update) => unreachable!(),
+    if let Some(command) = cli.command {
+        command.execute(cli.config).await?;
     }
 
     Ok(())
