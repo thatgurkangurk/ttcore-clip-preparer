8cfb5f7c7ec0436d7505d69203dde224ea888c72
Gurkan <hello@gurkz.me>
2026-03-03 13:30:13 +0100

    simplify cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -42,51 +42,44 @@ pub enum Commands {
 
 impl Commands {
     pub async fn execute(self, config_path: Option<PathBuf>) -> Result<()> {
+        if let Commands::Update = self {
+            update::update()?;
+            return Ok(());
+        }
+
+        // load config and client for all other commands
+        let config =
+            Config::load(config_path.as_deref()).context("failed to load configuration")?;
+        ensure_out_dir_exists(&config)
+            .await
+            .context("failed to ensure output directory exists")?;
+        let client = Client::new();
+
         match self {
-            Commands::Update => {
-                update::update()?;
-                return Ok(());
-            }
-
-            command => {
-                let config =
-                    Config::load(config_path.as_deref()).context("failed to load configuration")?;
-
-                ensure_out_dir_exists(&config)
+            Commands::Download { video_id } => {
+                download::download_selected_files(video_id, &config, &client)
                     .await
-                    .context("failed to ensure output directory exists")?;
-
-                match command {
-                    Commands::Download { video_id } => {
-                        let client = Client::new();
-
-                        download::download_selected_files(video_id, &config, &client)
-                            .await
-                            .context("download command failed")?;
-                    }
-
-                    Commands::BurnCredits { video_id, crf } => {
-                        let path = config.fs.out_dir.join(video_id.to_string());
-
-                        burner::burn_multiline_text_batch(path, config.fs.font_file, crf)
-                            .context("failed to burn credits text")?;
-                    }
-
-                    Commands::Clean => {
-                        clean_output_dir(&config)
-                            .await
-                            .context("failed to clean output directory")?;
-                    }
-
-                    Commands::CleanBurned => {
-                        clean_burned_dirs(&config.fs.out_dir)
-                            .await
-                            .context("failed to clean burned directories")?;
-                    }
-
-                    Commands::Update => unreachable!(),
-                }
+                    .context("download command failed")?;
             }
+            Commands::BurnCredits { video_id, crf } => {
+                burner::burn_multiline_text_batch(
+                    config.fs.out_dir.join(video_id.to_string()),
+                    config.fs.font_file,
+                    crf,
+                )
+                .context("failed to burn credits text")?;
+            }
+            Commands::Clean => {
+                clean_output_dir(&config)
+                    .await
+                    .context("failed to clean output directory")?;
+            }
+            Commands::CleanBurned => {
+                clean_burned_dirs(&config.fs.out_dir)
+                    .await
+                    .context("failed to clean burned directories")?;
+            }
+            Commands::Update => unreachable!(), // already handled
         }
 
         Ok(())
