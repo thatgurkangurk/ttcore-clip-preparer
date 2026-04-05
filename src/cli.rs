9757c52afbdfe076f54ee456e137b034d2565843
Gurkan <hello@gurkz.me>
2026-04-05 15:51:59 +0200

    add help information to the commands

--- src/cli.rs
+++ src/cli.rs
@@ -13,22 +13,33 @@ pub struct Cli {
 
 #[derive(Debug, Subcommand)]
 pub enum Commands {
+    /// download all clips marked as selected for a specific video
     Download {
         #[arg(long)]
         video_id: String,
     },
+    /// get the total number of clips for a specific video
     ClipCount {
         #[arg(long)]
         video_id: String,
     },
+    
+    /// burn credit information directly into the file
     BurnCredits {
         #[arg(long)]
         video_id: String,
         #[arg(long)]
         crf: Option<i32>,
     },
+    /// clean the `out` directory (deletes everything !)
     Clean,
+
+    /// clean the `out/burned` directory
     CleanBurned,
+
+    /// update the tool
     Update,
+
+    /// list all videos created on the frontend
     ListVideos,
 }
